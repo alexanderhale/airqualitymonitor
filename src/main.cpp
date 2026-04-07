c5284be79e4bda2161c7009e220d35d16f13d707
Alexander Hale <a.hale@live.ca>
2026-04-07 15:11:40 -0700

    wifi reconnection logic

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,17 @@ const char* password = WIFI_PASSWORD;
 
 WiFiServer server(80);
 
+#define WIFI_TIMEOUT_MS 30000
+#define WIFI_RECONNECT_MS 10000
+
+void connectWiFi() {
+  WiFi.begin(ssid, password);
+  unsigned long start = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - start < WIFI_TIMEOUT_MS) {
+    delay(500);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -29,9 +40,11 @@ void setup() {
   delay(2000);
 
   // WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  WiFi.setAutoReconnect(true);
+  connectWiFi();
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi connect failed, rebooting...");
+    ESP.restart();
   }
 
   server.begin();
@@ -43,30 +56,42 @@ float lastHumidity = NAN;
 bool pmsReady = false;
 PMS::DATA lastPms;
 
-String htmlPage() {
-  String page = "<!DOCTYPE html><html><head>";
-  page += "<meta http-equiv='refresh' content='5'>";
-  page += "<style>body{font-family:Arial;margin:40px;}h1{color:#333;}.error{color:#c00;}</style>";
-  page += "</head><body>";
-  page += "<h1>ESP32 Air Quality Station</h1>";
+static char pageBuf[1024];
+
+const char* htmlPage() {
+  char tempStr[64];
+  char humStr[64];
+  char pmsStr[256];
 
   if (isnan(lastTemp) || isnan(lastHumidity)) {
-    page += "<p class='error'>Temperature/Humidity sensor error</p>";
+    snprintf(tempStr, sizeof(tempStr), "<p class='error'>Temperature/Humidity sensor error</p>");
+    humStr[0] = '\0';
   } else {
-    page += "<p><b>Temperature:</b> " + String(lastTemp) + " &deg;C</p>";
-    page += "<p><b>Humidity:</b> " + String(lastHumidity) + " %</p>";
+    snprintf(tempStr, sizeof(tempStr), "<p><b>Temperature:</b> %.1f &deg;C</p>", lastTemp);
+    snprintf(humStr, sizeof(humStr), "<p><b>Humidity:</b> %.1f %%</p>", lastHumidity);
   }
 
   if (!pmsReady) {
-    page += "<p class='error'>Particulate sensor warming up...</p>";
+    snprintf(pmsStr, sizeof(pmsStr), "<p class='error'>Particulate sensor warming up...</p>");
   } else {
-    page += "<p><b>PM1.0:</b> " + String(lastPms.PM_AE_UG_1_0) + " &micro;g/m&sup3;</p>";
-    page += "<p><b>PM2.5:</b> " + String(lastPms.PM_AE_UG_2_5) + " &micro;g/m&sup3;</p>";
-    page += "<p><b>PM10:</b> " + String(lastPms.PM_AE_UG_10_0) + " &micro;g/m&sup3;</p>";
+    snprintf(pmsStr, sizeof(pmsStr),
+      "<p><b>PM1.0:</b> %u &micro;g/m&sup3;</p>"
+      "<p><b>PM2.5:</b> %u &micro;g/m&sup3;</p>"
+      "<p><b>PM10:</b> %u &micro;g/m&sup3;</p>",
+      lastPms.PM_AE_UG_1_0, lastPms.PM_AE_UG_2_5, lastPms.PM_AE_UG_10_0);
   }
 
-  page += "</body></html>";
-  return page;
+  snprintf(pageBuf, sizeof(pageBuf),
+    "<!DOCTYPE html><html><head>"
+    "<meta http-equiv='refresh' content='5'>"
+    "<style>body{font-family:Arial;margin:40px;}h1{color:#333;}.error{color:#c00;}</style>"
+    "</head><body>"
+    "<h1>ESP32 Air Quality Station</h1>"
+    "%s%s%s"
+    "</body></html>",
+    tempStr, humStr, pmsStr);
+
+  return pageBuf;
 }
 
 void readSensors() {
@@ -102,7 +127,7 @@ void handleClient() {
   String req = client.readStringUntil('\r');
   client.flush();
 
-  String page = htmlPage();
+  const char* page = htmlPage();
 
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
@@ -111,8 +136,20 @@ void handleClient() {
   client.println(page);
 }
 
+unsigned long lastWiFiCheck = 0;
+
 void loop() {
   readSensors();
   handleClient();
+
+  // Reconnect WiFi if dropped
+  if (millis() - lastWiFiCheck > WIFI_RECONNECT_MS) {
+    lastWiFiCheck = millis();
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("WiFi lost, reconnecting...");
+      connectWiFi();
+    }
+  }
+
   delay(100);
 }
